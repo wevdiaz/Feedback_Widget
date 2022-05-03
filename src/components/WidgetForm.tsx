6e8533daa232486bf5ece9f892de2151bedea460
Weverson Diazz <wevers.info@gmail.com>
2022-05-03 18:53:41 -0300

    aplicando estado e tipo para seleção de feedback e alterações de estado

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
 import thoughtImageUrl from "../assets/thought.svg";
-import { Key } from "phosphor-react";
 
 const feedbackTypes = {
     BUG: {
@@ -29,7 +29,11 @@ const feedbackTypes = {
     }
 }
 
+type feedbackType = keyof typeof feedbackTypes
+
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
            <header>
@@ -37,22 +41,28 @@ export function WidgetForm() {
                <CloseButton />
            </header>
 
-           <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => { 
-                    return (
-                        <button
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none "
-                            // onClick={}
-                            type="button"
-                        >
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    )
-                 })
-                }
-           </div>
+          {!feedbackType ? (
+               <div className="flex py-8 gap-2 w-full">
+               { Object.entries(feedbackTypes).map(([key, value]) => { 
+                   return (
+                       <button
+                           key={key}
+                           className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none "
+                           onClick={() => setFeedbackType(key as feedbackType)}
+                           type="button"
+                       >
+                           <img src={value.image.source} alt={value.image.alt} />
+                           <span>{value.title}</span>
+                       </button>
+                   )
+                })
+               }
+          </div>
+
+          ) : (
+              <p>Hello World!</p>
+          )
+        }
 
            <footer className="text-xs text-neutral-400">
                Feito com 💜 pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
