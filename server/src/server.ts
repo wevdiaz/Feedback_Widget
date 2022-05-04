cc173d2d70a7d2a9dd61b77fbf2b78d19eae3193
Weverson Diazz <wevers.info@gmail.com>
2022-05-04 17:47:49 -0300

    configurando o envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "8332721698dee7",
+      pass: "8498c37cf7c6e7"
+    }
+});
 
 app.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
             comment,
             screenshot
         }
+    });
+
+    await transport.sendMail({
+        from: "Equipe Feedget <atendente@feedget.com.br>",
+        to: "Weverson Diazz <wevers.info@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join("\n")
     });
     
     return res.status(201).json({ data: feedback });
