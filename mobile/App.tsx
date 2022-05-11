296f0ce130eacc6b4a7b5f098df08f1208b34d87
Weverson Diazz <wevers.info@gmail.com>
2022-05-11 18:44:53 -0300

    personalizando fonte com expo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_500Medium, Inter_400Regular } from '@expo-google-fonts/inter';
+
 import { theme } from './src/theme';
 import { Widget } from "./src/components/Widget";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
