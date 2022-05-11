bbae353b4f55a6679c0cb4b436d07647b8da24fe
Weverson Diazz <wevers.info@gmail.com>
2022-05-11 18:00:31 -0300

    criação do componente

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View } from "react-native";
+
+import { styles } from "./style";
+
+export function Widget() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
