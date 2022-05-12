a63975d3a0fecee9f9067bd5a11918801b8ae603
Weverson Diazz <wevers.info@gmail.com>
2022-05-11 21:47:12 -0300

    correções de instalações de pacotes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import  AppLoading from 'expo-app-loading';
@@ -22,13 +24,14 @@ export default function App() {
       backgroundColor: theme.colors.background
     }}>
 
-      <Widget />
-      
       <StatusBar 
           style="light"
           backgroundColor='transparent'
           translucent 
       />
+      
+      <Widget />
+      
     </View>
   );
 }
