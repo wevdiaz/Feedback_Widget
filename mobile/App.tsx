94f7ad14f410eb7ada2f6571f3d57f909fdab6a6
Weverson Diazz <wevers.info@gmail.com>
2022-05-23 18:19:55 -0300

    instalando e configurando fontes personalizadas no app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,38 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import { Widget } from './src/components/Widget';
+import { theme } from './src/theme';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World direto do React Native</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      
+      <Text style={{color: "white", fontSize: 20}}>Hello World direto do React Native</Text>
+
+      <StatusBar 
+          style="light"
+          backgroundColor='transparent'
+          translucent 
+      />
+      <Widget />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
