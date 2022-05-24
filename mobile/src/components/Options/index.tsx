d8002683621525f09c24c9ce3045bdc35dbf9a56
Weverson Diazz <wevers.info@gmail.com>
2022-05-24 17:50:53 -0300

    criando as opções e suas estilização

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { Copyright } from "../Copyright";
+import { Option } from "../Option";
 
+import { feedbackTypes } from "../../utils/feedbackTypes";
 import { styles } from "./styles";
 
 export function Options() {
     return (
         <View style={styles.container}>
+            <Text style={styles.title} >Deixe seu Feedback</Text>
+
+            <View style={styles.options}
+            >
+                {
+                    Object.entries(feedbackTypes).map(([key, value]) => {
+                       return <Option 
+                            key={key}
+                            title={value.title}
+                            image={value.image}
+                        />
+                    })
+                }
+
+            </View>
 
             <Copyright />
 
