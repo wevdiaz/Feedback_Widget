a7ab6ebaee219b6d8f280cf809c174b3b7ff3389
Weverson Diazz <wevers.info@gmail.com>
2022-05-28 15:47:05 -0300

    aplicando o loading no botão de envio e desabilitando o mesmo enquanto envia

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { feedbackType, feedbackTypes } from "..";
 import { api } from "../../../libs/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -17,11 +18,14 @@ export function FeedbackContentStep({
     onFeedbackSent }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null);
     const [comment, setComment] = useState("");
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     async function handleSubmitFeedback(e : FormEvent) {
         e.preventDefault();
+
+        setIsSendingFeedback(true);
 
         // console.log({
         //     screenshot,
@@ -34,6 +38,7 @@ export function FeedbackContentStep({
             screenshot
         });
         
+        setIsSendingFeedback(false);
         onFeedbackSent();
     }
 
@@ -73,10 +78,10 @@ export function FeedbackContentStep({
 
                     <button
                         type="submit"
-                        disabled = {comment.length == 0}
+                        disabled = {comment.length == 0 || isSendingFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                        Enviar feedback
+                        { isSendingFeedback ? <Loading /> : "Enviar feedback" }
                     </button>
                 </footer>
             </form>
